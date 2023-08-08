2a32a60ae43412627c57d9403fe629914c6bd73f
Jacob Franco <jacob704@live.unc.edu>
2023-08-08 12:01:47 -0400

    Initial Commit

---
+++ Apollo/ApolloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApolloApp.swift
+//  Apollo
+//
+//  Created by Jacob Franco on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ApolloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
